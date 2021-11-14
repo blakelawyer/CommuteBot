93443391f4ff8799d6ac3b199319a81a8c27d66c
Alex Schittko <aschittko@thrivepos.com>
2021-11-14 02:58:33 -0600

    Enable cloudwatch logs

--- ci/ci.sh
+++ ci/ci.sh
@@ -5,7 +5,7 @@ set -x
 commit_sha=$(git rev-parse --short HEAD)
 repo="397766053761.dkr.ecr.us-east-2.amazonaws.com/comet-commute"
 tag="${repo}:${commit_sha}"
-bash build.sh $tag
+bash build.sh $tag $repo
 
 if [[ "$TRAVIS_BRANCH" != "main" ]]; then   
     echo "aborting - I only deploy for main!"
