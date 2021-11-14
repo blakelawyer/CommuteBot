218c360fbaab3f73d06c66c0b94e318be91b4c44
Alex Schittko <aschittko@thrivepos.com>
2021-11-14 02:51:43 -0600

    Enable cloudwatch logs

--- ci/terraform-app/main.tf
+++ ci/terraform-app/main.tf
@@ -12,6 +12,8 @@ provider "aws" {
     region = "us-east-2"
 }
 
+data "aws_region" "current" {}
+
 locals { 
     app_name = "comet-commute"
 }
@@ -94,29 +96,36 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attach
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# resource "aws_iam_role" "ecs_task_role" {
-#   name = "${local.app_name}-ecsTaskRole"
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${local.app_name}-ecsTaskRole"
  
-#   assume_role_policy = <<EOF
-# {
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Action": "sts:AssumeRole",
-#      "Principal": {
-#        "Service": "ecs-tasks.amazonaws.com"
-#      },
-#      "Effect": "Allow",
-#      "Sid": ""
-#    }
-#  ]
-# }
-# EOF
-# }
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
-#   role       = aws_iam_role.ecs_task_role.name
-# }
+resource "aws_iam_role_policy_attachment" "ecs_task_policy_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = local.app_name
+}
+
 
 resource "aws_ecs_task_definition" "this" {
   network_mode             = "awsvpc"
@@ -125,12 +134,17 @@ resource "aws_ecs_task_definition" "this" {
   cpu                      = 256
   memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  //task_role_arn            = aws_iam_role.ecs_task_role.arn
-  container_definitions = jsonencode([{
-   name        = "${local.app_name}"
-   image       = "${var.container_image}"
-   essential   = true
-}])
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  container_definitions = templatefile(
+  "${path.module}/task-definition.json", {
+    "name" = local.app_name,
+    "image" = var.container_image,
+    "ram" = 512,
+    "cpu" = 256
+    "region" = data.aws_region.current.name,
+    "app_name" = local.app_name
+  })
+
 }
 
 
